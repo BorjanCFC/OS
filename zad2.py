edec65aa025e1c0e7bd0bba9117a3b5fdf519f03
Borjan Petrevski <120339726+BorjanCFC@users.noreply.github.com>
2024-11-06 18:21:19 +0100

    Update zad2.py

--- zad2.py
+++ zad2.py
@@ -1,5 +1,4 @@
 import sys
-
 from collections import defaultdict
 
 def print_dates(file_path, flag):
@@ -18,14 +17,14 @@ def print_dates(file_path, flag):
             print('\n'.join(items))
             print()
     elif flag == '-f':
-        for date, items in dates.items():
-            print(f"Datum: {date} - {len(items)} ставки")
-            for item in items:
+        for date, items in dates:
+            print(f"Datum: {date} - {len(dates[date])} ставки")
+            for item in dates[date]:
                 print(item)
             print()
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Upotreba: python script.py <putanja_datoteke> <-d|-f>")
+        print("Upotreba: python script.py <pateka_na_datoteka> <-d|-f>")
         sys.exit(1)
     print_dates(sys.argv[1], sys.argv[2])
