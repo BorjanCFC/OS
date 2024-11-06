2469db582eacfeb53e892eb14df26b1fda317bf8
Borjan Petrevski <120339726+BorjanCFC@users.noreply.github.com>
2024-11-06 18:23:05 +0100

    Update zad2.py

--- zad2.py
+++ zad2.py
@@ -1,15 +1,17 @@
 import sys
 from collections import defaultdict
 
-def print_dates(file_path, flag):
+def print_creation_modification_dates(file_path, flag):
     with open(file_path, 'r') as file:
-        data = [line.strip().split() for line in file.readlines() if len(line.strip().split()) >= 8]
+        lines = file.readlines()
 
     dates = defaultdict(list)
-    for line in data:
-        date = ' '.join(line[5:8])
-        item = ' '.join(line[8:])
-        dates[date].append(item)
+    for line in lines:
+        parts = line.strip().split()
+        if len(parts) >= 8:
+            date = ' '.join(parts[5:8])
+            item = ' '.join(parts[8:])
+            dates[date].append(item)
 
     if flag == '-d':
         for date, items in dates.items():
@@ -17,14 +19,15 @@ def print_dates(file_path, flag):
             print('\n'.join(items))
             print()
     elif flag == '-f':
-        for date, items in dates:
-            print(f"Datum: {date} - {len(dates[date])} ставки")
-            for item in dates[date]:
-                print(item)
-            print()
+        for date, items in dates.items():
+            for item in items:
+                print(f"{item} - {date}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Upotreba: python script.py <pateka_na_datoteka> <-d|-f>")
+        print("Upotreba: python script.py <putanja_datoteke> <-d|-f>")
         sys.exit(1)
-    print_dates(sys.argv[1], sys.argv[2])
+
+    file_path = sys.argv[1]
+    flag = sys.argv[2]
+    print_creation_modification_dates(file_path, flag)
