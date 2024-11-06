a03d2f8d4cfe7ec809e818df43784cc957262d22
Borjan Petrevski <120339726+BorjanCFC@users.noreply.github.com>
2024-11-06 20:52:39 +0100

    Update zad3.c

--- zad3.c
+++ zad3.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Upotreba: %s <broj_izvrsuvanja> <komanda> [argumenti]\n", argv[0]);
+        printf("Usage: %s command [args...]\n", argv[0]);
         return 1;
     }
 
-    int num_executions = atoi(argv[1]);
-    if (num_executions < 1) {
-        printf("Broj izvrsuvanja mora da bide pozitiven broj!\n");
-        return 1;
-    }
+    int N;
+    printf("Vnesi broj na izvrsuvanja: ");
+    scanf("%d", &N);
 
-    for (int i = 0; i < num_executions; i++) {
-        pid_t pid = fork();
-        if (pid == 0) { // Dete proces
-            execvp(argv[2], &argv[2]);
-            printf("Greska pri izvrsuvanje na komanda\n");
-            return 1;
-        } else if (pid > 0) { // Roditel proces
-            int status;
-            waitpid(pid, &status, 0);
-        } else { // Greshka pri kreiranje na proces
-            printf("Greska pri kreiranje na proces\n");
-            return 1;
+    for (int i = 0; i < N; i++) {
+        pid_t dete = fork();
+
+        if (dete < 0) {
+            printf("fork failed");
+            return 0;
+        }
+        else if (dete == 0) {  
+
+            execvp(argv[1], argv + 1);
+            
+            printf("Dete failed to exec\n");
+            return 0;
         }
     }
 
-    printf("Zavrshuva proces 0\n");
+    for (int i = 0; i < N; i++) {
+        wait(NULL);
+    }
+
+    printf("Zavrshuva process 0\n");
     return 0;
+    
 }
